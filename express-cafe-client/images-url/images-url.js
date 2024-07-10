a5c5b45ea660a9671baad8d35edc3e4704bc23af
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-10 09:22:06 -0500

    added some random image url for user input in the add a coffee form

---
+++ express-cafe-client/images-url/images-url.js
@@ -0,0 +1,9 @@
+/**
+ * Just added some image url to test if it works
+ * https://www.pexels.com/photo/close-up-of-coffee-cup-on-table-312418/
+ * https://www.pexels.com/photo/brown-liquid-pouring-on-black-and-white-ceramic-mug-selective-color-photography-1235706/
+ * https://www.pexels.com/photo/panda-printed-paper-coffee-cup-on-table-885021/
+ * https://www.pexels.com/photo/white-ceramic-mug-with-coffee-4264049/
+ * https://www.pexels.com/photo/glass-cup-with-liquid-3556686/
+ * https://www.pexels.com/photo/white-ceramic-mug-and-saucer-685527/
+ */
