573d297202e11529c88e5dab83146ee0ee0e2107
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-12 10:05:15 -0500

    Create Home component to display list of coffee cards using fetched data

--- express-cafe-client/src/pages/Home.jsx
+++ express-cafe-client/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import CoffeeCard from '../components/CoffeeCard';
 
 const Home = () => {
+  const coffees = useLoaderData();
+
   return (
-    <div className="container mx-auto mt-4">
-      
-      <h1 className="text-2xl font-bold">Welcome to Express Cafe</h1>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Coffee Menu</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {coffees.map((coffee) => (
+          <CoffeeCard key={coffee._id} coffee={coffee} />
+        ))}
+      </div>
     </div>
   );
 };
