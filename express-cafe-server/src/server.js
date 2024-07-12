a738f612097128e01d4070ed5cffdb247b10a0f5
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-12 10:37:40 -0500

    Add DELETE /coffee/:id route to delete coffee entries by ID

--- express-cafe-server/src/server.js
+++ express-cafe-server/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { MongoClient, ObjectId } = require('mongodb'); // Ensure ObjectId is imported
+const { MongoClient, ObjectId } = require('mongodb'); 
 
 dotenv.config();
 
