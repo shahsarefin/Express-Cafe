6b6ac07a817f27f6926664a0cee6dee4a6c6cf20
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-12 10:04:51 -0500

    Set up routing with react-router-dom and define loader function for fetching coffee data

--- express-cafe-client/src/main.jsx
+++ express-cafe-client/src/main.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
 import AddCoffee from './pages/AddCoffee';
 import UpdateCoffee from './pages/UpdateCoffee';
-import Navbar from './components/Navbar';
 import './index.css';
+
+// Loader function to fetch coffee data
+const coffeeLoader = async () => {
+  const response = await fetch('http://localhost:5001/coffees');
+  if (!response.ok) {
+    throw new Error('Failed to fetch coffee data');
+  }
+  return response.json();
+};
+
+// Define the routes with the loader
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} loader={coffeeLoader} />
+      <Route path="add-coffee" element={<AddCoffee />} />
+      <Route path="update-coffee" element={<UpdateCoffee />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/add-coffee" element={<AddCoffee />} />
-        <Route path="/update-coffee" element={<UpdateCoffee />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
