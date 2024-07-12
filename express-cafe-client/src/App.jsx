53975a6371cd7b2db12f759747c31bce43f433bf
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-12 10:04:05 -0500

    Set up App component to include Navbar and use Outlet for routing

--- express-cafe-client/src/App.jsx
+++ express-cafe-client/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home'
+import React from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Home></Home>
+      <Navbar />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
