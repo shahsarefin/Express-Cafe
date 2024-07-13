414ada93ca44a65a7763c03ce47fa33130727bcd
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-13 18:37:25 -0500

    navbar design updated

--- express-cafe-client/src/components/Navbar.jsx
+++ express-cafe-client/src/components/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   return (
     <nav className="bg-brown-800 p-4">
       <div className="container mx-auto flex justify-between">
-        <Link to="/" className="text-white text-lg font-bold fancy-font">Express Cafe</Link>
+        <Link to="/" className="text-white text-lg font-bold">Express Cafe</Link>
         <div>
           <Link to="/add-coffee" className="text-white mx-2">Add Coffee</Link>
+          <Link to="/sign-up" className="text-white mx-2">Sign Up</Link>
+          <Link to="/sign-in" className="text-white mx-2">Sign In</Link>
         </div>
       </div>
     </nav>
