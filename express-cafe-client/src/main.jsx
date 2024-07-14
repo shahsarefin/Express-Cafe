3afc622551f16f81d36d04e06c81d7114acaaeca
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-13 20:12:17 -0500

    added users to route

--- express-cafe-client/src/main.jsx
+++ express-cafe-client/src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import AddCoffee from './pages/AddCoffee';
 import UpdateCoffee from './pages/UpdateCoffee';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Users from './pages/Users';
 import './index.css';
 import { AuthProvider } from './provider/AuthProvider';
 
-// Loader function to fetch coffee data from the server 
+// Loader function to fetch coffee data from the server
 const coffeeLoader = async () => {
   const response = await fetch('http://localhost:5001/coffees');
   if (!response.ok) {
@@ -19,7 +20,7 @@ const coffeeLoader = async () => {
   return response.json();
 };
 
-// routes
+// Routes
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
       <Route path="update-coffee/:id" element={<UpdateCoffee />} />
       <Route path="sign-up" element={<SignUp />} />
       <Route path="sign-in" element={<SignIn />} />
+      <Route path="users" element={<Users />} />
     </Route>
   )
 );
